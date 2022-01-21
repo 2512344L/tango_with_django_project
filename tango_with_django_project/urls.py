58ac696b12883fcb63ee53fc5ac32a53295af35e
Yuqi Li <2512344l@student.gla.ac.uk>
2022-01-21 23:44:52 +0800

    second week

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    path('rango/', views.index, name='index'),
+    path('rango/about/', views.about, name = 'about'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
