e5a0f710bcc1352741b009a0a255d9cf768fc6a9
Yuqi Li <2512344l@student.gla.ac.uk>
2022-01-29 01:24:45 +0800

    third week

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from rango.models import Category, Page
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Page, PageAdmin)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,8 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('rango/', views.index, name='index'),
-    path('rango/about/', views.about, name = 'about'),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+                  path('', views.index, name='index'),
+                  path('rango/', include('rango.urls')),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
